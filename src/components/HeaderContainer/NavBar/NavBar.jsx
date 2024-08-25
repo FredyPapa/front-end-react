6d4c26e6a55ecca77f7d385a68ee4927c9f8aba6
Fredy Papa <fredy.luis.wolf@gmail.com>
2024-08-25 14:07:02 -0500

    Proyecto papa-web-store-2024 con metodologia BEM

--- src/components/HeaderContainer/NavBar/NavBar.jsx
+++ src/components/HeaderContainer/NavBar/NavBar.jsx
@@ -15,26 +15,26 @@ function NavBar() {
           <NavLink to="/"><Logo/></NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto barraNav">
-              <NavLink className="menuBar menuLaterales" activeClassName="menuBarActive" to="/inicio">Inicio</NavLink>
-              <NavDropdown title="Servicios" id="basic-nav-dropdown" className="menuBar">
+            <Nav className="me-auto navbar__nav">
+              <NavLink className="navbar__nav__navlink menuLaterales" activeClassName="navbar__nav__navlink--active" to="/inicio">Inicio</NavLink>
+              <NavDropdown title="Servicios" id="basic-nav-dropdown" className="navbar__nav__navlink">
                 <NavDropdown.Item>
-                  <NavLink className="subMenuBar" to="/catalogo/1">Sitios Web</NavLink>
+                  <NavLink className="navbar__nav__navlink__subMenuBar" to="/catalogo/1">Sitios Web</NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item>
-                  <NavLink className="subMenuBar" to="/catalogo/2">Comercio electrónico</NavLink>
+                  <NavLink className="navbar__nav__navlink__subMenuBar" to="/catalogo/2">Comercio electrónico</NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item>
-                  <NavLink className="subMenuBar" to="/catalogo/3">Aplicaciones Web</NavLink>
+                  <NavLink className="navbar__nav__navlink__subMenuBar" to="/catalogo/3">Aplicaciones Web</NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item>
-                  <NavLink className="subMenuBar" to="/catalogo/4">Aplicaciones Móviles</NavLink>
+                  <NavLink className="navbar__nav__navlink__subMenuBar" to="/catalogo/4">Aplicaciones Móviles</NavLink>
                 </NavDropdown.Item>
               </NavDropdown>
-              <NavLink className="menuBar menuLaterales" activeClassName="menuBarActive" to="/contacto">Contáctenos</NavLink>
+              <NavLink className="navbar__nav__navlink menuLaterales" activeClassName="navbar__nav__navlink--active" to="/contacto">Contáctenos</NavLink>
             </Nav>
           </Navbar.Collapse>
           {items.length?<NavLink to="/carrito"><CartWidget/></NavLink>:<></>}
